b2ab19ca2f8d2f9141207e33f01a5eca3632f765
Jadson Santos <jadson44.santos@gmail.com>
2021-05-10 18:21:56 -0300

    vapix: Fix to request open door in the versions old A1001

--- custom_components/vapix/__init__.py
+++ custom_components/vapix/__init__.py
@@ -1,14 +1,36 @@
 from requests import post
 from requests.auth import HTTPDigestAuth
-
+from homeassistant.core import HomeAssistant
 import json
 
 from .const import (
     DOMAIN
 )
 
+def setup(hass:HomeAssistant, config):
 
-def setup(hass, config):
+    def get_door_list(call):
+        ip = call.data.get("ip", "")
+        username = call.data.get("username", "")
+        password = call.data.get("password", "")
+        data = {"axtdc:GetDoorList":{}}
+       
+        r=post("http://{ip}/vapix/doorcontrol".format(ip=ip),
+        json.dumps(data),
+        auth=HTTPDigestAuth(username, password))
+      
+        if r.status_code == 200:
+            e = json.loads(r.text)
+
+            for k in e['Door']:
+                hass.services.call(
+                domain="notify",
+                service="persistent_notificationt",
+                service_data={ "title": k['Name'], "message": k['token'] })
+
+            return True
+        else:
+            return False
 
     def open_remote_door(call):
         ip = call.data.get("ip", "")
@@ -20,15 +42,16 @@ def setup(hass, config):
             "Content-Type": "application/json",
         }
 
-        payload = {
-            "tdc:AccessDoor":  {
-                "Token": doorid
-            }
-        }
+        payload = {"tdc:AccessDoor":  {"Token": doorid } }
 
-        post("http://" + username + ":" + password + "@" + ip + "/vapix/doorcontrol",
-                        data=json.dumps(payload), headers=headers, auth=HTTPDigestAuth(username, password))
+        post(
+            "http://{ip}/vapix/doorcontrol".format(ip=ip),
+            data=json.dumps(payload),
+            headers=headers,
+            auth=HTTPDigestAuth(username, password)
+        )
 
     hass.services.register(DOMAIN, "open_remote_door", open_remote_door)
-
+    hass.services.register(DOMAIN, "get_door_list", get_door_list)
+    
     return True
